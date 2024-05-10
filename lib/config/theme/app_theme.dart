26f179f9cad978149430a3f954c07447ea04affa
josh_MacBook <josuearroliga89@gmail.com>
2024-05-10 10:02:52 -0600

    Added app theme and screen page

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.orange,
+  Colors.red,
+  Colors.purple
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less or equal than ${colorList.length - 1}');
+
+//Crete the method to get the theme data.
+  ThemeData getTheme() => ThemeData(
+        colorSchemeSeed: colorList[selectedColor],
+      );
+}
