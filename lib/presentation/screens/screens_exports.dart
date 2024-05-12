39ecc1687894d4b8be86111ad0b0824f5ff512fb
josh_MacBook <josuearroliga89@gmail.com>
2024-05-12 13:43:56 -0600

    Screens created and barrel upated.

--- lib/presentation/screens/screens_exports.dart
+++ lib/presentation/screens/screens_exports.dart
@@ -1,5 +1,9 @@
 //Page created to have easy access to the many screens imports to avoid importing all of them in the go router page.
-
+export 'package:widget_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
+export 'package:widget_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widget_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widget_app/presentation/screens/snackbar/snack_bar_screen.dart';
+export 'package:widget_app/presentation/screens/ui_controls/ui_controls_screen.dart';
